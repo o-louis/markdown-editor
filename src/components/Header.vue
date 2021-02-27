64f0d9c1a84f62e166ce7c6366b2b6a8cce4e1f9
o-louis <oriane.louis.pro@gmail.com>
2021-02-27 00:38:54 -0500

    ✨ Create Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header
+    class="w-full h-14 bg-black text-white px-6 flex items-center text-xl"
+  >
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
