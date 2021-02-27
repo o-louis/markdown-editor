0b1f9a81bcc843d7d573e9408a954cf4da55ad8e
o-louis <oriane.louis.pro@gmail.com>
2021-02-27 00:39:10 -0500

    ✨ Create Markdown component

---
+++ src/components/Markdown.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-96 mt-8 px-4">
+    <form class="h-full overflow-scroll border border-black w-1/2 ">
+      <textarea name="markdown" class="w-full h-full border-0"></textarea>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Markdown",
+};
+</script>
