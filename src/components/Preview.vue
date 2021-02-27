67c06d4ac49edf16487f3a1e95ca06b2795c18d6
o-louis <oriane.louis.pro@gmail.com>
2021-02-27 18:40:00 -0500

    ✨ Create Preview component

---
+++ src/components/Preview.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-1/2 h-full max-w-2xl p-4 overflow-y-auto border border-black">
+    {{ content }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Preview",
+  props: {
+    content: String,
+  },
+};
+</script>
