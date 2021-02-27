5f18c839d156ea146339bb30c2922d7b3eba5ee0
o-louis <oriane.louis.pro@gmail.com>
2021-02-27 18:34:17 -0500

    🎨 Add semicolon in main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './assets/style/tailwind.css'
+import Vue from 'vue';
+import App from './App.vue';
+import './assets/style/tailwind.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
