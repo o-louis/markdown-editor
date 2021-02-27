355b9f886b906c69b6933818bc5c53bc30d81fde
o-louis <oriane.louis.pro@gmail.com>
2021-02-27 00:40:07 -0500

    ✨ Add the Header and the Markdown components to the App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="app"></div>
+  <div id="app" class="min-h-screen">
+    <Header title="Markdown Editor" />
+    <Markdown />
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Markdown from "@/components/Markdown";
 export default {
   name: "App",
+  components: {
+    Header,
+    Markdown,
+  },
 };
 </script>
