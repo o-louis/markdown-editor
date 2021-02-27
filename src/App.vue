7cccf092c6a6c9657d6f02cbafe13443bc8239a7
o-louis <oriane.louis.pro@gmail.com>
2021-02-27 18:41:12 -0500

    ✨ Get content from Markdown and display in Preview

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app" class="min-h-screen">
     <Header title="Markdown Editor" />
-    <section class="px-6 py-4 h-section">
-      <Markdown />
+    <section class="flex justify-between px-6 py-4 h-section">
+      <Markdown @getHTMLContent="setContent" />
+      <Preview :content="content" />
     </section>
   </div>
 </template>
@@ -10,11 +11,23 @@
 <script>
 import Header from "@/components/Header";
 import Markdown from "@/components/Markdown";
+import Preview from "@/components/Preview";
 export default {
   name: "App",
   components: {
     Header,
     Markdown,
+    Preview,
+  },
+  data() {
+    return {
+      content: "",
+    };
+  },
+  methods: {
+    setContent(value) {
+      this.content = value;
+    },
   },
 };
 </script>
--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,8 +1,12 @@
 <template>
-  <form class="w-1/2 h-full max-w-2xl overflow-y-auto border border-black">
+  <form
+    class="w-1/2 h-full max-w-2xl overflow-y-auto border border-black"
+    submit.prevent
+  >
     <textarea
       name="markdown"
       class="w-full h-full p-4 border-0 outline-none"
+      v-model="input"
     ></textarea>
   </form>
 </template>
@@ -10,5 +14,15 @@
 <script>
 export default {
   name: "Markdown",
+  data() {
+    return {
+      input: "",
+    };
+  },
+  watch: {
+    input: function() {
+      this.$emit("getHTMLContent", this.input);
+    },
+  },
 };
 </script>
