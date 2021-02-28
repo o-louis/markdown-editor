cd6622a06bf02e33b7d4286b9c46c12f7e7f2891
o-louis <oriane.louis.pro@gmail.com>
2021-02-27 19:06:39 -0500

    ✨ Compile HTML to Markdown and print it

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="w-1/2 h-full max-w-2xl p-4 overflow-y-auto border border-black">
-    {{ content }}
-  </div>
+  <div
+    class="w-1/2 h-full max-w-2xl px-4 overflow-y-auto border border-black markdown"
+    v-html="compiledMarkdown"
+  />
 </template>
 
 <script>
+const marked = require("marked");
+
 export default {
   name: "Preview",
   props: {
     content: String,
+  },
+  computed: {
+    compiledMarkdown() {
+      return marked(this.content);
+    },
   },
 };
 </script>
